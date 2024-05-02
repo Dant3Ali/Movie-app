f41a286028afecccf08382c2f37124f4aa5a8150
Khodjaev Doryush <101807515+Dant3Ali@users.noreply.github.com>
2024-05-02 16:22:29 +0300

    Update MovieDao.java

--- src/main/java/com/movie/proj/dao/MovieDao.java
+++ src/main/java/com/movie/proj/dao/MovieDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface MovieDao {
+
     public void add(Movie movie);
     public void addAll(List<Movie> movies);
     public String serializeMovie(Movie movie);
